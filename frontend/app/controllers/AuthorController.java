18244230b85f03c3f18ea942ec1a5f560bbce262
harshasanne <sriharsha.sanne@sv.cmu.edu>
2018-11-01 13:25:43 -0700

    Added req 5&6 frontend and back end

--- frontend/app/controllers/AuthorController.java
+++ frontend/app/controllers/AuthorController.java
@@ -1,5 +1,6 @@
 package controllers;
-
+import play.data.Form;
+import play.data.FormFactory;
 import javax.inject.Inject;
 import java.net.URLEncoder;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -8,12 +9,17 @@ import util.APICall;
 import util.Constants;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.*;
+import views.html.paperYear;
+import models.*;
+
 public class AuthorController extends Controller {
     private APICall apiCall;
+    private final FormFactory formFactory;
 
     @Inject
-    public AuthorController(APICall apiCall) {
+    public AuthorController(FormFactory formFactory,APICall apiCall) {
         this.apiCall = apiCall;
+        this.formFactory = formFactory;
     }
 
     public Result getPapers(String name) throws Exception {
@@ -23,10 +29,39 @@ public class AuthorController extends Controller {
         String jstring = nodes.toString();
         String p = new String();
         List<String> paperList = new ArrayList<String>();
+        if(nodes != null){
         for(int i = 0;i < nodes.size();i++){
            p = nodes.get(i).findPath("title").asText();
            paperList.add(p);
         }
+    }
         return ok(views.html.author.render(paperList));
+    }
+    public Result publicationPerYear() {
+        Form<author> authorForm = formFactory.form(author.class);
+        Form<Paper> paperForm = formFactory.form(Paper.class);
+        return ok(views.html.paperYear.render(authorForm,paperForm));
+    }
+      public Result getPapersYear() throws Exception {
+        Form<author> authName = formFactory.form(author.class).bindFromRequest();
+        Paper paperForm = formFactory.form(Paper.class).bindFromRequest().get();
+        String authorName = authName.get().name.replace(" ", "%20");
+       //System.out.println(paperForm.startYear+"........");        // TODO: We shouldn't hard code url here. someone needs to refactor this code to Constants.java
+        JsonNode nodes = apiCall.callAPI(Constants.BACKEND + "/autho" + "/" + authorName+"?from="+paperForm.startYear+"&to="+paperForm.endYear);
+    
+        return ok();
+    }
+
+     public Result journalAuthors() {
+        Form<Paper> paperForm = formFactory.form(Paper.class);
+        return ok(views.html.journalHistogram.render(paperForm));
+    }
+      public Result getJournalAuthors() throws Exception {
+        Form<Paper> paperForm = formFactory.form(Paper.class).bindFromRequest();
+        String name = paperForm.get().journalName.replace(" ", "%20");
+       System.out.println(name+"........");        // TODO: We shouldn't hard code url here. someone needs to refactor this code to Constants.java
+        JsonNode nodes = apiCall.callAPI(Constants.BACKEND + "/journal" + "/" + name);
+    
+        return redirect(routes.HomeController.index());
     }
 }
