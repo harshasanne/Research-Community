76d10ff1b16211b3a3a08c479b0219bfc3c501c8
ptfang <131220148@smail.nju.edu.cn>
2018-11-02 00:58:39 -0700

    some changes on rqmt 10

---
+++ frontend/app/controllers/FindResearcherByKeywordController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import javax.inject.Inject;
+import java.net.URLEncoder;
+import com.fasterxml.jackson.databind.JsonNode;
+import play.mvc.*;
+import util.APICall;
+import util.Constants;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class FindResearcherByKeyword {
+}
