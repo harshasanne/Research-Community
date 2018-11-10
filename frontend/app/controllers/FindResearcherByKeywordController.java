d2e3554ab44e14987284551bad472bff9a0e3a5b
harshasanne <sriharsha.sanne@sv.cmu.edu>
2018-11-09 16:26:46 -0800

    Advance 2 is done

--- frontend/app/controllers/FindResearcherByKeywordController.java
+++ frontend/app/controllers/FindResearcherByKeywordController.java
@@ -41,5 +41,22 @@ public class FindResearcherByKeywordController extends Controller{
 
         return ok(views.html.singleInput.render("researcherByKeyword","keyword","Find researchers by keyword and interests"));
     }
+    public Result getResearchers(String keyword) throws Exception {
+        // TODO: We shouldn't hard code url here. someone needs to refactor this code to Constants.java
+        JsonNode nodes = apiCall.callAPI(Constants.BACKEND + "/ResearcherByKeyword" + "/" + URLEncoder.encode(keyword, "UTF-8"));
+        // TODO: Harsha, you may want to change the return value a bit to fit into your frontend UI
+        //String jstring = nodes.toString();
+        System.out.println(nodes);
+
+
+
+
+        return ok(views.html.researchersByKeyword.render(nodes,keyword));
+    }
+
+    public Result getResearchersForm() throws Exception {
+
+        return ok(views.html.researchrsKeywordForm.render("researchersByKeyword","keyword","Find researchers by keyword and interests"));
+    }
 }
 
