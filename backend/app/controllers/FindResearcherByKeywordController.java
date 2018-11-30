76bbf9edbd34cf7dabbc57f29de1291fe9add3dd
ptfang <131220148@smail.nju.edu.cn>
2018-11-30 13:00:53 -0800

    rqmt 11 on researcherByKeyword

--- backend/app/controllers/FindResearcherByKeywordController.java
+++ backend/app/controllers/FindResearcherByKeywordController.java
@@ -18,6 +18,8 @@ import com.typesafe.config.Config;
 import javax.inject.Inject;
 import java.io.*;
 
+import static org.neo4j.driver.v1.Values.parameters;
+
 public class FindResearcherByKeywordController extends Controller{
     private Config config;
 
@@ -26,31 +28,27 @@ public class FindResearcherByKeywordController extends Controller{
         this.config = config;
     }
 
-    public Result getResearcher(String keywords) throws Exception{
+    public Result getResearcher(String keywords,String authorName) throws Exception{
         keywords = URLDecoder.decode(keywords, "UTF-8");
 
-//
-//        String[] keyList= keywords.split(",");
-//        ArrayList<String> finalKeywordList = new ArrayList();
-//        for(String k:keyList)
-//        {
-//            finalKeywordList.add(k);
-//        }
 
-        String query = "MATCH(a:Author)-[r:HAS_KEYWORD]->(k:Keyword) " + "WHERE a.RI =~ '.*"+keywords+".*' or k='" + keywords +
-                "' RETURN a.authorName, count(a.authorName) as c\n" +
-                "ORDER BY c desc\n" +
-                "limit 10";
-        System.out.println(query);
-//        Map<String, List<String>> map = new HashMap<>();
-//        map.put("finalList", finalKeywordList);
-//        org.neo4j.ogm.model.Result result = DBConnector.session().query(query, map);
-//        Iterator<Map<String, Object>> keywordIterator = result.iterator();
-//        while (keywordIterator.hasNext()) {
-//            String author = keywordIterator.next().get("author").toString();
-//            authors.add(author);
-//        }
+        String[] tempList= keywords.split(",");
+        ArrayList<String> keyList = new ArrayList();
+        for(String k:tempList)
+        {
+            keyList.add(k);
+        }
 
+        String query1 = "MATCH (author:Author)-[:WRITES]->(p:Paper)-[r:HAS_KEYWORD]->(key:Keyword) WHERE key.keyword IN {keyList} RETURN author.authorName as author,COUNT(p.title) as cnt  ORDER BY cnt DESC Limit 5";
+
+        String query3 = "MATCH (author:Author)-[s:searched]->(sk:searchedKey) WHERE sk.keyword IN {keyList} RETURN author.authorName";
+        String query4 = "MATCH (author:Author)-[s:searched]->(sk:searchedKey) WHERE author.authorName IN {keyAuthorList} RETURN sk.keyword, count(sk.keyword) as cnt  ORDER BY cnt DESC Limit 5";
+
+        System.out.println(query1);
+
+        Map<String, Object> params = new HashMap<>();
+
+        params.put("keyList", keyList);
 
 
 //3D Medical Volume Reconstruction Using Web Services.
@@ -62,26 +60,159 @@ public class FindResearcherByKeywordController extends Controller{
                 @Override
                 public List<String> execute( Transaction tx )
                 {
-                    return findResearcher( tx, query );
+                    return findResearcher( tx, query1, "keyList",keyList );
                 }
             } );
+            System.out.println("reauthors:" + authors);
+
+            for(String keyword: keyList) {
+                String query2 = "MERGE (a:Author{authorName:'" + authorName + "'}) MERGE(sk:searchedKey{keyword:'" + keyword + "'}) ON CREATE SET sk.cnt = 1 ON MATCH SET sk.cnt = sk.cnt + 1 MERGE (a)-[s:searched]->(sk)    return sk.cnt";
+                List<String> scounts = session.writeTransaction(new TransactionWork<List<String>>() {
+                    @Override
+                    public List<String> execute(Transaction tx) {
+                        return mergeSearchedKey(tx, query2);
+                    }
+                });
+            }
+
+
+            List<String> keyAuthorList =  session.readTransaction( new TransactionWork<List<String>>()
+            {
+                @Override
+                public List<String> execute( Transaction tx )
+                {
+                    return findKeyAuthor( tx, query3, "keyList",keyList );
+                }
+            } );
+            System.out.println(keyAuthorList);
+
+
+
+            Set<String> hs = new HashSet<>();
+            hs.addAll(keyAuthorList);
+            keyAuthorList.clear();
+            keyAuthorList.addAll(hs);
+            System.out.println(keyAuthorList);
+
+
+            List<String> searchedKeys =  session.readTransaction( new TransactionWork<List<String>>()
+            {
+                @Override
+                public List<String> execute( Transaction tx )
+                {
+                    return findSearchedKeys( tx, query4, "keyAuthorList",keyAuthorList );
+                }
+            } );
+
+            System.out.println(searchedKeys);
+
             List<Author> authorObjects = new ArrayList<Author>();
             for (String a : authors) {
                 authorObjects.add(new Author(a));
             }
-            System.out.println(authorObjects.get(0));
-            return ok(new Gson().toJson(authorObjects));
+            //System.out.println(authorObjects.get(0));
+
+            JsonArray keysArray = new JsonArray();
+            for(String k: searchedKeys)
+            {
+                JsonPrimitive element = new JsonPrimitive(k);
+                keysArray.add(element);
+            }
+
+            JsonArray authorsArray = new JsonArray();
+            for(String a: authors)
+            {
+                JsonPrimitive element = new JsonPrimitive(a);
+                authorsArray.add(element);
+            }
+
+            // System.out.println("jarray:" + jArray);
+            JsonParser parser = new JsonParser();
+            JsonObject obj = new JsonObject();
+
+            //obj = parser.parse(authors.get(0)).getAsJsonObject();
+            obj.add("searchedKeys", keysArray);
+            obj.add("authorNames", authorsArray);
+
+            //System.out.println(obj.toString());
+            System.out.println("jarray:" + keysArray);
+            System.out.println("obj:" + obj);
+            //System.out.println("gson :" + gson);
+            return ok(new Gson().toJson(obj)).as("applications/json");
+
+            //return ok(new Gson().toJson(authorObjects));
         }
     }
 
-    private static List<String> findResearcher(Transaction tx, String query)
+    private static List<String> findResearcher(Transaction tx, String query, String paramsName, ArrayList keyList)
     {
         List<String> authors = new ArrayList<>();
-        StatementResult result = tx.run( query );
+        StatementResult result = tx.run( query,parameters( paramsName, keyList));
+
+
         while ( result.hasNext() )
         {
-            authors.add(result.next().get(0).asString());
+
+            //System.out.println(result.next());
+            String resString = result.next().get(0).asString();
+            authors.add(resString);
+            //System.out.println(resString);
+            //System.out.println(result.next().get(0).asString());
         }
         return authors;
     }
+
+    private static List<String> findKeyAuthor(Transaction tx, String query, String paramsName, ArrayList keyList)
+    {
+        List<String> keyAuthor = new ArrayList<>();
+        StatementResult result = tx.run( query,parameters( paramsName, keyList));
+
+
+        while ( result.hasNext() )
+        {
+
+            //System.out.println(result.next());
+            String resString = result.next().get(0).asString();
+            keyAuthor.add(resString);
+           // System.out.println(resString);
+            //System.out.println(result.next().get(0).asString());
+        }
+        return keyAuthor;
+    }
+
+    private static List<String> mergeSearchedKey(Transaction tx, String query)
+    {
+        List<String> metaDatas = new ArrayList<>();
+        StatementResult result = tx.run( query );
+        Gson gson = new Gson();
+        while ( result.hasNext() )
+        {
+
+            Record record = result.next();
+            String recordString = gson.toJson(record.asMap());
+
+            metaDatas.add(recordString);
+        }
+        return metaDatas;
+    }
+
+    private static List<String> findSearchedKeys(Transaction tx, String query, String paramsName, List keyList)
+    {
+        List<String> searchedKeys = new ArrayList<>();
+        StatementResult result = tx.run( query,parameters( paramsName, keyList));
+
+
+        while ( result.hasNext() )
+        {
+
+
+            String resString = result.next().get(0).asString();
+            searchedKeys.add(resString);
+           // System.out.println(resString);
+
+        }
+        return searchedKeys;
+    }
+
+
 }
